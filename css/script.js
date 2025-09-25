d2e154eb5bbe37c1e7c8579b0714bab991bc888e
AaAn7hony <anthonyfreile.est2@gmail.com>
2025-09-25 00:05:45 -0500

    Add files via upload

--- css/script.js
+++ css/script.js
@@ -21,3 +21,35 @@ header.onmouseout = function() {
         header.classList.add("scroll");
     }
 };
+
+
+const toggleBtn = document.getElementById('tema');
+const temaIcon = document.getElementById('tema-icon');
+
+// comprobar preferencia guardada
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'light') {
+  document.body.classList.add('light');
+  temaIcon.src = 'img/dark_mode_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg';
+} else {
+  temaIcon.src = 'img/light_mode_24dp_E3E3E3_FILL1_wght400_GRAD0_opsz24.svg';
+}
+
+toggleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  document.body.classList.toggle('light');
+
+  if (document.body.classList.contains('light')) {
+    localStorage.setItem('theme', 'light');
+    temaIcon.src = 'img/dark_mode_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg';
+  } else {
+    localStorage.setItem('theme', 'dark');
+    temaIcon.src = 'img/light_mode_24dp_E3E3E3_FILL1_wght400_GRAD0_opsz24.svg';
+  }
+});
+
+
+
+
